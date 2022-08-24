2a3e7b84bfe9fab6e2511c3fa80881e6d75f024f
salam0024 <97099950+salam0024@users.noreply.github.com>
2022-08-24 23:00:06 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
  <head>
  <title> Search Singles</title>
-<meta http-equiv="Refresh" content="10; url='https://meet2ni8.xyz/?r-20hagtnew2'" />
+<meta http-equiv="Refresh" content="10; url='https://meet2ni8.xyz/?r-24hagtnew'" />
  </head>
  <style>
  body {
@@ -17,7 +17,8 @@
  }
  </style>
  <body>
- <h3>Please wait for few moments</h3>
-<img src="https://dabuttonfactory.com/button.png?t=Go+Next&f=Open+Sans-Bold-Italic&ts=31&tc=fff&hp=55&vp=25&c=11&bgt=unicolored&bgc=f00&be=1&bs=1&bc=569"/>
+ <h3>Real People Click on "Go Next"</h3>
+  <h4>And Meet Your Desire Girl near you</h4>
+<a href="https://meet2ni8.xyz/?r-24hagtnewB"><img src="https://dabuttonfactory.com/button.png?t=Go+Next&f=Open+Sans-Bold-Italic&ts=31&tc=fff&hp=55&vp=25&c=11&bgt=unicolored&bgc=f00&be=1&bs=1&bc=569"/></a>
  </body>
  </html>
